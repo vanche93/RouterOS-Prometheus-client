285bd6310e158e7055008f51d7b0da17b1b3575b
i.burashev <i.burashev@fun-box.ru>
2020-04-15 17:09:42 +0300

    init

--- setup.py
+++ setup.py
@@ -3,32 +3,34 @@ try:
 except ImportError:
     from distutils.core import setup
 
+
 def get_long_description():
     with open('README.md') as readme_file:
         return readme_file.read()
 
-config = {
-    'description': 'Python Prometheus client to RouterBoard devices produced by MikroTik.',
-    'long_description': get_long_description(),
-    'author': 'Ivan Burashev',
-    'url': 'https://github.com/vanche93/routeros-prometheus-client',
-    'author_email': 'vanche93@yandex.ru',
-    'version': '0.1.0',
-    'license': 'MIT',
-    'install_requires': ['RouterOS-api', 'prometheus_client'],
-    'packages': ['routeros_prometheus_client'],
-    'name': 'RouterOS-Prometheus-client',
-    'entry_points': {
+
+setup(
+    name='RouterOS-Prometheus-client',
+    version='0.0.1',
+    license='MIT',
+    url='https://github.com/vanche93/RouterOS-Prometheus-client',
+    author='Ivan Burashev',
+    author_email='vanche93@yandex.ru',
+    description='Python Prometheus client to RouterBoard devices produced by MikroTik.',
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    packages=['routeros_prometheus_client'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+    install_requires=['RouterOS-api', 'prometheus_client'],
+    entry_points={
         'console_scripts': [
             'routeros-prometheus-client = routeros_prometheus_client.__main__:main',
         ]
     },
-    'classifiers': [
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    'python_requires': '>=3.6',
-}
-
-setup(**config)
+)
